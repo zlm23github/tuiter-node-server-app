6b5ed4a4017eac03a43a0103b72b9c938b438d21
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-14 17:13:39 -0400

    1

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const schema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: String,
+  password: String,
   firstName: String,
   lastName: String,
 }, { collection: "users" });
