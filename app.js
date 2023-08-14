d99a6f47ce38045d7c829788db179110f7efb8dd
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-14 14:49:50 -0400

    1

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const app = express()
 app.use(cors(
     {
         credentials: true,
-        origin: process.env.FRONTEND_URL
+        // origin: process.env.FRONTEND_URL
+        origin: process.env.REACT_APP_SERVER_API_URL
     }
 ));
 const sessionOptions = {
