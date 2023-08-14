614cc5a0674b08c32c730fe77c09af36f7e5815f
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-14 12:39:35 -0400

    1

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING);
 
 
