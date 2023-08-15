3b307cd8ff8abdcf659f9256285dc0db79cd563e
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-15 17:10:06 -0400

    1

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors(
     {
         credentials: true,
         origin: process.env.FRONTEND_URL
-        
+        // 'http://localhost:3000'
         // process.env.FRONTEND_URL
     }
 ));
